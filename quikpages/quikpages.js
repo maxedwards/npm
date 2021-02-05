3c7a1c87cf26682b8bdca05a15f1523755bcdbc9
maxedwards <max.edwards.uk@gmail.com>
2021-02-05 20:21:46 +0100

    max@ThinkCentre2:Fri 05 Feb 2021 08:21:46 PM CET

---
+++ quikpages/quikpages.js
@@ -0,0 +1,13 @@
+console.log("This will be the public verison of quikpages.js")
+// package will also include:
+// - catalogue.2.js
+// - pages.2.js
+// - evalInScope.js
+// - fraggle.js
+
+// package will NOT include:
+// - FOX.js (dependency)
+// - ensureFolder.js (dependency)
+// - stopwatch.js (dependency)
+// - protolibs.js (dependency)
+// - safeBraces.js [DEPRACATED]
