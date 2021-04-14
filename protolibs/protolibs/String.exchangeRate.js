3b95ed1b90407c6bc1f952664d43af18b956b91c
maxedwards <max.edwards.uk@gmail.com>
2021-04-14 12:25:54 +0100

    max@EliteBook:Wed 14 Apr 2021 12:25:54 PM BST

--- protolibs/protolibs/String.exchangeRate.js
+++ protolibs/protolibs/String.exchangeRate.js
@@ -1,26 +1,16 @@
-const exchangeCache=[];
+const cache=[];
 //WITH:String.getJSON
 module.exports=function(to,freshSeconds=60){
     freshSeconds=Math.max(parseFloat(freshSeconds)||0,60);
     return new Promise(async (ok,fail)=>{
         let slug=`${this.toString()}-${to}`;
         let now=new Date().valueOf()/1000, staleBefore=now-freshSeconds;
-        //console.log({freshSeconds,staleBefore});
-
-        if(exchangeCache[slug]&&(exchangeCache[slug].t>=staleBefore)) {
-            //console.log('String.exchangeRate.js CACHE HIT');
-            return ok(exchangeCache[slug].v);
-        }
-
-        // if(exchangeCache[slug])console.log('Stale cache:',exchangeCache[slug].t);
+        if(cache[slug]&&(cache[slug].t>=staleBefore))  return ok(cache[slug].v);
         console.log('String.exchangeRate is calling API..');
-
         `https://api.cryptonator.com/api/ticker/${slug}`.$getJSON()
             .then(x=>{
                 if(!x.success) throw 'API failure:'+x.error;
-                x.timestamp=now;
-                x=exchangeCache[slug]={t:now,v:x.ticker.price};
-                //console.log(x);
+                x=cache[slug]={t:now,v:x.ticker.price};
                 ok(x.v);
             })
             .catch(fail);
